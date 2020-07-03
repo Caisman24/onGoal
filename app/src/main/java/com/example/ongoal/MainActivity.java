191d621530b13e53c3921d6c0bce6dcf891255b5
Caisman <caiussuciu@gmail.com>
2020-07-03 14:42:46 +0300

    Added branding icon and register view

--- app/src/main/java/com/example/ongoal/MainActivity.java
+++ app/src/main/java/com/example/ongoal/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.ongoal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tvSignUp = findViewById(R.id.tvSignUp);
+        TextView tvSignUpBottom = findViewById(R.id.tvSignUpBottom);
+        tvSignUp.setOnClickListener(onClickSignUp());
+        tvSignUpBottom.setOnClickListener(onClickSignUp());
+    }
+
+    private View.OnClickListener onClickSignUp(){
+        return new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, Register.class);
+                MainActivity.this.startActivity(intent);
+            }
+        };
     }
 }
---
+++ app/src/main/java/com/example/ongoal/Register.java
@@ -0,0 +1,23 @@
+package com.example.ongoal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        TextView tvLogin = findViewById(R.id.tvLogin);
+        tvLogin.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Register.this.finish();
+            }
+        });
+    }
+}
