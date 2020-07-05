231aca8c42c4deae7db35e196f72d814d5362e67
Caisman <caiussuciu@gmail.com>
2020-07-05 13:57:42 +0300

    Restructured the activities

--- app/src/main/java/com/example/ongoal/MainActivity.java
+++ app/src/main/java/com/example/ongoal/MainActivity.java
@@ -1,28 +1,17 @@
 package com.example.ongoal;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-    EditText editTextEmail, editTextPassword;
-    Button buttonLogin;
-    FirebaseAuth auth;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,69 +20,15 @@ public class MainActivity extends AppCompatActivity {
 
         Objects.requireNonNull(getSupportActionBar()).hide();
 
-        TextView tvSignUp = findViewById(R.id.tvSignUp);
-        TextView tvSignUpBottom = findViewById(R.id.tvSignUpBottom);
-        tvSignUp.setOnClickListener(onClickSignUp());
-        tvSignUpBottom.setOnClickListener(onClickSignUp());
-
-        editTextEmail = findViewById(R.id.editTextEmail);
-        editTextPassword = findViewById(R.id.editTextPassword);
-
         auth = FirebaseAuth.getInstance();
-        FirebaseAuth.AuthStateListener authStateListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser firebaseUser = auth.getCurrentUser();
-                if (firebaseUser != null) {
-                    Toast.makeText(MainActivity.this, "You are logged in", Toast.LENGTH_SHORT).show();
-                    Intent intentHome = new Intent(MainActivity.this, MainActivity.class);
-                    startActivity(intentHome);
-                } else {
-                    Toast.makeText(MainActivity.this, "Please enter your credentials", Toast.LENGTH_SHORT).show();
-                }
-            }
-        };
-
-        buttonLogin = findViewById(R.id.buttonLogin);
     }
 
-    private void loginUser() {
-        String email = editTextEmail.getText().toString().trim();
-        String password = editTextPassword.getText().toString().trim();
-
-        if (email.isEmpty()) {
-            editTextEmail.setError("Email is required");
-            editTextEmail.requestFocus();
-            return;
+    @Override
+    protected void onStart() {
+        super.onStart();
+        final FirebaseUser currentUser = auth.getCurrentUser();
+        if (currentUser == null) {
+            Intent notLogged = new Intent(getApplicationContext(), Login.class);
         }
-
-        if (password.isEmpty()) {
-            editTextPassword.setError("Password is required");
-            editTextPassword.requestFocus();
-            return;
-        }
-
-        // if email and password is not empty
-        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (!task.isSuccessful()) {
-                    Toast.makeText(MainActivity.this, "Login Error, Please loggin Again", Toast.LENGTH_SHORT).show();
-                } else {
-                    Intent intentHome = new Intent(MainActivity.this, MainActivity.class);
-                    startActivity(intentHome);
-                }
-            }
-        });
-
-    }
-
-    private View.OnClickListener onClickSignUp() {
-        return new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Register.class);
-                MainActivity.this.startActivity(intent);
-            }
-        };
     }
 }
